8be5565258a86890c88e36cf25a37d08ca3e18ed
tux <mathieu.barrios@outlook.com>
2024-09-23 21:15:03 +0200

    changer les menus

--- web_app/views.py
+++ web_app/views.py
@@ -9,11 +9,8 @@ app.wsgi_app = ProxyFix(
     app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
 )
 
-liste_categorie = ('Accueil','Réseaux','Web','Data','About')
-liste_cotes_reseaux =('Autohébergement','Monitoring','Kube','Proxy','Certificat')
-liste_cotes_web =('HTML','Flask','Jinja','Gunicorn','Podman')
-liste_cotes_data =('SQL','Python','Pandas','Graphique')
-
+menu = (['Accueil','Accueil'],["Réseaux",'Autohébergement','Monitoring','Kube','Proxy','Certificat'],['Web','HTML','Flask','Jinja','Gunicorn','Podman']
+    ,['Data','SQL','Python','Pandas','Graphique'],['About','About'])
 
 @app.route("/robots.txt")
 def static_from_root():
@@ -22,155 +19,90 @@ def static_from_root():
 @app.route("/", methods=["GET", "POST"])
 @app.route("/index/", methods=["GET", "POST"])
 def Accueil():
-    return render_template("index.html",liste_categorie=liste_categorie,categorie=liste_categorie[0])
+    return render_template("index.html",menu=menu)
 
 ####################################################### Partie RESEAUX  ###############################################################
 
 @app.route("/reseaux/", methods=["GET", "POST"])
 def Réseaux():
-    return render_template("reseaux_intro.html"
-                            ,categorie=liste_categorie[1]
-                            ,liste_categorie=liste_categorie
-                            ,liste_cotes_categorie=liste_cotes_reseaux)
+    return render_template("reseaux_intro.html",menu=menu)
 
 
 @app.route("/reseaux/Auto-hebergement/", methods=["GET", "POST"])
 def Autohébergement():
-    return render_template("reseaux/autohebergement.html"
-                            ,categorie=liste_categorie[1]
-                            ,liste_categorie=liste_categorie
-                            ,liste_cotes_categorie=liste_cotes_reseaux
-                            ,categorie_cotes=liste_cotes_reseaux[0])
+    return render_template("reseaux/autohebergement.html",menu=menu)
 
 
 @app.route("/reseaux/Monitoring/", methods=["GET", "POST"])
 def Monitoring():
-    return render_template("reseaux/monitoring.html"
-                            ,categorie=liste_categorie[1]
-                            ,liste_categorie=liste_categorie
-                            ,liste_cotes_categorie=liste_cotes_reseaux
-                            ,categorie_cotes=liste_cotes_reseaux[1])
+    return render_template("reseaux/monitoring.html",menu=menu)
 
 @app.route("/reseaux/PodmanKube/", methods=["GET", "POST"])
 def Kube():
-    return render_template("encours.html"
-                            ,categorie=liste_categorie[1]
-                            ,liste_categorie=liste_categorie
-                            ,liste_cotes_categorie=liste_cotes_reseaux
-                            ,categorie_cotes=liste_cotes_reseaux[2])
+    return render_template("encours.html",menu=menu)
 
 @app.route("/reseaux/Proxy/", methods=["GET", "POST"])
 def Proxy():
-    return render_template("encours.html"
-                            ,categorie=liste_categorie[1]
-                            ,liste_categorie=liste_categorie
-                            ,liste_cotes_categorie=liste_cotes_reseaux
-                            ,categorie_cotes=liste_cotes_reseaux[3])
+    return render_template("encours.html",menu=menu)
 
 @app.route("/reseaux/Certificat/", methods=["GET", "POST"])
 def Certificat():
-    return render_template("reseaux/certificats.html"
-                            ,categorie=liste_categorie[1]
-                            ,liste_categorie=liste_categorie
-                            ,liste_cotes_categorie=liste_cotes_reseaux
-                            ,categorie_cotes=liste_cotes_reseaux[4])
+    return render_template("reseaux/certificats.html",menu=menu)
 
 
 ####################################################### Partie WEB  ###################################################################
 
 @app.route("/web/", methods=["GET", "POST"])
 def Web():
-    return render_template("web_intro.html"
-                            ,categorie=liste_categorie[2]
-                            ,liste_categorie=liste_categorie
-                            ,liste_cotes_categorie=liste_cotes_web
+    return render_template("web_intro.html",menu=menu
                             )
 
 
 @app.route("/web/HTMLCSS", methods=["GET", "POST"])
 def HTML():
-    return render_template("web/htmlcss.html"
-                            ,categorie=liste_categorie[2]
-                            ,liste_categorie=liste_categorie
-                            ,liste_cotes_categorie=liste_cotes_web
-                            ,categorie_cotes=liste_cotes_web[0])
+    return render_template("web/htmlcss.html",menu=menu)
 
 
 @app.route("/web/flask", methods=["GET", "POST"])
 def Flask():
-    return render_template("web/Flask.html"
-                            ,categorie=liste_categorie[2]
-                            ,liste_categorie=liste_categorie
-                            ,liste_cotes_categorie=liste_cotes_web
-                            ,categorie_cotes=liste_cotes_web[1])
+    return render_template("web/Flask.html",menu=menu)
 
 @app.route("/web/jinja", methods=["GET", "POST"])
 def Jinja():
-    return render_template("encours.html"
-                            ,categorie=liste_categorie[2]
-                            ,liste_categorie=liste_categorie
-                            ,liste_cotes_categorie=liste_cotes_web
-                            ,categorie_cotes=liste_cotes_web[2])
+    return render_template("encours.html",menu=menu)
 
 @app.route("/web/gunicorn", methods=["GET", "POST"])
 def Gunicorn():
-    return render_template("encours.html"
-                            ,categorie=liste_categorie[2]
-                            ,liste_categorie=liste_categorie
-                            ,liste_cotes_categorie=liste_cotes_web
-                            ,categorie_cotes=liste_cotes_web[3])
+    return render_template("encours.html",menu=menu)
 
 @app.route("/web/podman", methods=["GET", "POST"])
 def Podman():
-    return render_template("encours.html"
-                            ,categorie=liste_categorie[2]
-                            ,liste_categorie=liste_categorie
-                            ,liste_cotes_categorie=liste_cotes_web
-                            ,categorie_cotes=liste_cotes_web[4])
+    return render_template("encours.html",menu=menu)
 
 ####################################################### Partie DATA  ##################################################################
 
 @app.route("/data/", methods=["GET", "POST"])
 def Data():
-    return render_template("data_intro.html"
-                            ,categorie=liste_categorie[3]
-                            ,liste_categorie=liste_categorie
-                            ,liste_cotes_categorie=liste_cotes_data)
+    return render_template("data_intro.html",menu=menu)
 
 @app.route("/data/SQL", methods=["GET", "POST"])
 def SQL():
-    return render_template("data/sql.html"
-                            ,categorie=liste_categorie[3]
-                            ,liste_categorie=liste_categorie
-                            ,liste_cotes_categorie=liste_cotes_data
-                            ,categorie_cotes=liste_cotes_data[0])
+    return render_template("data/sql.html",menu=menu)
 
 @app.route("/data/python", methods=["GET", "POST"])
 def Python():
-    return render_template("data/python.html"
-                            ,categorie=liste_categorie[3]
-                            ,liste_categorie=liste_categorie
-                            ,liste_cotes_categorie=liste_cotes_data
-                            ,categorie_cotes=liste_cotes_data[1])
+    return render_template("data/python.html",menu=menu)
 
 @app.route("/data/pandas", methods=["GET", "POST"])
 def Pandas():
-    return render_template("encours.html"
-                            ,categorie=liste_categorie[3]
-                            ,liste_categorie=liste_categorie
-                            ,liste_cotes_categorie=liste_cotes_data
-                            ,categorie_cotes=liste_cotes_data[2])
+    return render_template("encours.html",menu=menu)
 
 @app.route("/data/graphique", methods=["GET", "POST"])
 def Graphique():
-    return render_template("encours.html"
-                            ,categorie=liste_categorie[3]
-                            ,liste_categorie=liste_categorie
-                            ,liste_cotes_categorie=liste_cotes_data
-                            ,categorie_cotes=liste_cotes_data[3])
+    return render_template("encours.html",menu=menu)
 
 ####################################################### Partie ABOUT  ################################################################
 
 @app.route("/about/", methods=["GET", "POST"])
 def About():
-    return render_template("about.html",categorie=liste_categorie[4],liste_categorie=liste_categorie)
+    return render_template("about.html",menu=menu)
