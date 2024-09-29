2ab7904ac0bf0488697d53cfc0013d15cc706708
tux <mathieu.barrios@outlook.com>
2024-09-29 20:10:51 +0200

    automatisation WIP

---
+++ test.sh
@@ -0,0 +1,15 @@
+git checkout test
+git pull
+python -m venv venv ;
+source venv/bin/activate ;
+pip install -r requirements.txt ;
+pytest ;
+test=echo$? ;
+if ((test == 0));then
+    git checkout main;
+    git merge test;
+    git push origin main;
+    exit 0;     
+else
+    exit 1;
+fi
