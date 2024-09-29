8ba6e217456735346e272ec66c765aef4d687dc6
tux <mathieu.barrios@outlook.com>
2024-09-29 18:59:29 +0200

    enlevé une ligne sur base.html et ajouter des truc a faire

---
+++ pushTest.sh
@@ -0,0 +1,5 @@
+git push origin dev
+git checkout test
+git merge dev 
+git push origin test
+curl --user "tux:$TOKENJENKINS" "http://0.0.0.0:8090/job/piweb_deploy/buildWithParameters?token=$TOKENJENKINS"
